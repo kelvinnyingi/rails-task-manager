d15b6cf5bded04b6fd249cc24f0dac1ed88897d2
Kelvin Nyingi <adikelvin1@gmail.com>
2024-11-12 16:50:29 +0100

    task mnger

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,4 +5,26 @@ class TasksController < ApplicationController
   def show
     @tasks = Task.find(params[:id])
   end
+  def new
+    @task = Task.new
+  end
+  def create
+    @task =   Task.new(params[:task])
+    @task.save
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params[:task])
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def tasks_params
+    params.require(task).permit(:title, :details, :completed)
+  end
 end
